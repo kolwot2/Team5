660f3459d64ae80261435e32564ed83792a3111d
kolwot2 <pavel.shesnovich1@yandex.ru>
2020-12-06 18:14:24 +0300

    Added server connection

--- Railway/headers/ByteConverter.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-std::string toBytes(int x);
-std::string toBytes4(size_t x);
-std::string toBytes(size_t x);
---
+++ Railway/headers/ServerConnection.cpp
@@ -0,0 +1,41 @@
+#include "ServerConnection.h"
+#include <iostream>
+
+ServerConnection::ServerConnection() {
+	sf::Socket::Status status = socket.connect("wgforge-srv.wargaming.net", 443);
+	if (status != sf::Socket::Done) {
+		std::cout << "Couldn't connect to the server";
+	}
+}
+
+ServerConnection::~ServerConnection() {
+	socket.disconnect();
+}
+
+void ServerConnection::send(const ActionMessage &actionMessage) {
+	int type = actionMessage.actionCode;
+	socket.send(reinterpret_cast<char*>(&type), sizeof(type));
+	int length = actionMessage.data.size();
+	socket.send(reinterpret_cast<char*>(&length), sizeof(length));
+	socket.send(actionMessage.data.c_str(), (size_t)length);
+}
+
+ResposeMessage ServerConnection::recieve() {
+	int type, length;
+	std::string str_data = "";
+	size_t recieved;
+	socket.receive(&type, sizeof(type), recieved);
+	socket.receive(&length, (size_t)4, recieved);
+	size_t sum_recieved = 0;
+	char *data = new char[recieveMaxSize + 1];
+	while (sum_recieved < length) {
+		socket.receive(data, recieveMaxSize, recieved);
+		sum_recieved += recieved;
+		data[recieved] = 0;
+		std::string part_data(data);
+		str_data += part_data;
+	}
+	delete [] data;
+	ResposeMessage response((Result)type, str_data);
+	return response;
+}
